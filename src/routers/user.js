2ed1a64d22000d77a1823066eac852dd8d16b966
ManarArabi <manar.araby.ma@gmail.com>
2019-12-27 09:00:18 +0200

    login is added

--- src/routers/user.js
+++ src/routers/user.js
@@ -4,13 +4,23 @@ const User = require('../models/user')
 const router = new express.Router()
 
 router.post('/sign-up', async (req, res) => {
-	//TODO
-	//generate token and return it
 	const user = new User(req.body)
 	try {
 		await user.save()
 		res.status(201).send(user)
 	} catch (e) {
+		res.status(400).send(e)
+	}
+})
+
+router.post('/login', async (req, res) => {
+	try {
+		const user = await User.login(req.body.email, req.body.password)
+		const token = await user.generateToken()
+
+		res.send({ user, token })
+	} catch (e) {
+		console.log(e)
 		res.status(400).send(e)
 	}
 })
