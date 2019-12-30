f9fc75221475ddd27b1aeac59686f167190726af
ManarArabi <manar.araby.ma@gmail.com>
2019-12-30 14:17:23 +0200

    item router is added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 require('./db/mongoose')
 const express = require('express')
 const user_router = require('./routers/user')
+const item_router = require('./routers/item')
 
 const app = express()
 const port = 3000
 
 app.use(express.json())
 app.use(user_router)
+app.use(item_router)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
