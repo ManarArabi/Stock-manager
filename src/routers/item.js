a3f46eeef06a1621d5456b9c033d099a74fc1acd
ManarArabi <manar.araby.ma@gmail.com>
2020-01-29 17:00:54 +0200

    delete item is added - not working

--- src/routers/item.js
+++ src/routers/item.js
@@ -9,14 +9,31 @@ router.post('/item', auth, async (req, res) => {
 		name: req.body.name,
 		counter: req.body.counter,
 		category: req.body.category,
-		user: req.user._id
+		owner: req.user._id
 	})
 	try {
 		await item.save()
 		res.status(201).send(item)
 	} catch (e) {
-		console.log(e)
-		res.status(400).send(e)
+		res.status(400).send()
+	}
+})
+
+//not working
+router.delete('/item/:id', auth, async (req, res) => {
+	const _id = req.params.id
+	try {
+		const item = await Item.findOne({ _id })
+		console.log(typeof item.owner)
+		console.log(typeof req.user._id)
+		console.log(item.owner.equals(req.user._id))
+		if (item.owner.equals(req.user._id)) {
+			res.status(403).send("You aren't the owner of this item, you can't delete it.")
+		}
+		item = await Item.findOneAndDelete({ _id })
+		res.status(200).send(item)
+	} catch (e) {
+		res.status(400).send()
 	}
 })
 
