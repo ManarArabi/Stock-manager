7a8e1326342f27ccbf577f97a6eef4fbacc3393c
ManarArabi <manar.araby.ma@gmail.com>
2020-01-29 22:33:01 +0200

    category router is added

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@ global.mongoose = require('./db/mongoose');
 const express = require('express')
 const user_router = require('./routers/user')
 const item_router = require('./routers/item')
-const category_router = require('/routers/category')
+const category_router = require('./routers/category')
 
 const app = express()
 const port = process.env.PORT;
 
 app.use(express.json())
+
 app.use(user_router)
 app.use(item_router)
 app.use(category_router)
