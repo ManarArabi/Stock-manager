6379b4ae62bb2a0b9b09a8638733aff4fda97d1e
ManarArabi <manar.araby.ma@gmail.com>
2020-01-29 22:25:43 +0200

    category router is added

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ global.mongoose = require('./db/mongoose');
 const express = require('express')
 const user_router = require('./routers/user')
 const item_router = require('./routers/item')
+const category_router = require('/routers/category')
 
 const app = express()
 const port = process.env.PORT;
@@ -10,6 +11,7 @@ const port = process.env.PORT;
 app.use(express.json())
 app.use(user_router)
 app.use(item_router)
+app.use(category_router)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
