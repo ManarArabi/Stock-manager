acadf0fdec2fade44e717a067ce5ac87fa4ec2e5
ManarArabi <manar.araby.ma@gmail.com>
2020-01-29 22:21:41 +0200

    init commit

---
+++ src/models/category.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const categorySchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	parentCategory: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		default: null
+	},
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true
+	}
+}, { autoIndex: false });
+
+categorySchema.index({ parentCategory: 1, name: 1 }, { unique: true });
+
+const Category = mongoose.model('Category', categorySchema)
+
+module.exports = Category
