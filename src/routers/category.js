8c88f5aa8348a403378f5876112662a09205ade4
ManarArabi <manar.araby.ma@gmail.com>
2020-01-30 20:13:36 +0200

    add category is added

--- src/routers/category.js
+++ src/routers/category.js
@@ -1,7 +1,21 @@
-const category = require('./../models/category')
+const Category = require('./../models/category')
 const express = require('express')
 const auth = require('../middleware/auth')
 
 const router = new express.Router()
+
+router.post('/category', auth, async (req, res) => {
+	const category = new Category({
+		name: req.body.name,
+		parentCategory: req.body.counter,
+		owner: req.user._id
+	})
+	try {
+		await category.save()
+		res.status(201).send(category)
+	} catch (e) {
+		res.status(400).send(e)
+	}
+})
 
 module.exports = router
